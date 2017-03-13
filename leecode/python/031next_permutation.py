d9add4adf07f24e8ea12b87256b85d74126e5a74
kay <kayfire@163.com>
2017-03-13 09:04:06 +0800

    hive hadoop leecode

--- leecode/python/031next_permutation.py
+++ leecode/python/031next_permutation.py
@@ -1,4 +1,4 @@
-"""
+""" M
  Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
 
 If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
@@ -10,4 +10,5 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 3,2,1 → 1,2,3
 1,1,5 → 1,5,1
 """
+# TODO fix this, don't know what this theme meaning. 
 
