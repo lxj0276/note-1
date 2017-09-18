fb434b6540c9b3800924e437c566bfcf334d29ac
kay <kayfire@163.com>
2017-09-18 21:04:28 +0800

    Modify pirce graph

--- stock/codes/price_graph.py
+++ stock/codes/price_graph.py
@@ -12,11 +12,18 @@ from datetime import datetime
 from matplotlib.dates import DayLocator, HourLocator, DateFormatter, drange
 
 # ts.get_sina_dd('300053', date='2017-9-13')
-pd.set_option('display.height', 1000)
-pd.set_option('display.max_rows', 5000)
-pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
-fig = plt.figure()  
+# pd.set_option('display.height', 1000)
+# pd.set_option('display.max_rows', 5000)
+# pd.set_option('display.max_columns', 500)
+# pd.set_option('display.width', 1000)
+
+def get_open_date(start_date, end_date):
+    dates = ts.trade_cal()
+    filter_dates = dates[(dates.calendarDate >  start_date) & (dates.calendarDate < end_date)]
+    open_dates = filter_dates.query('isOpen == 1')
+    for _, date in open_dates['calendarDate'].iteritems():
+        yield date
+fig = plt.figure()
 ax_price = fig.add_subplot(1,1,1)
 def main():
     time_price = ts.get_tick_data('300675', date='2017-09-15')
@@ -28,7 +35,7 @@ def main():
     # print time_price
     # ax_price.xaxis.set_major_formatter( DateFormatter('%H:%M:%S') )
     # ax_price.fmt_xdata = DateFormatter('%H:%M:%S')
-    ax_volume = ax_price.twinx()
+    ax_volume = ax_px()
 
     time_price['time'] = pd.to_datetime(time_price['time'], format='%H:%M:%S')
     plt.gcf().autofmt_xdate()
